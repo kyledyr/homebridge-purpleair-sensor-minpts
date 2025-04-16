92661cfd1e1bab4ac68c99d19aba98cefd0ce0b5
kyledyr <53024646+kyledyr@users.noreply.github.com>
2025-04-16 10:59:30 -0700

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,12 +1,12 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'PurpleAirSensor';
+export const PLATFORM_NAME = 'PurpleAirSensorMinPts';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-purpleair-sensor';
+export const PLUGIN_NAME = 'homebridge-purpleair-sensor-minpts';
 
 
 /**
